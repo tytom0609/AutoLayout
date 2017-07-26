872cdbb8a43e82bf76f9085be6b4f84b716139fa
Ty Tom <tytom@Tys-MacBook-Pro.local>
2017-07-26 14:36:15 -0500

    Initial

---
+++ AutoLayout/SafetyViewController.swift
@@ -0,0 +1,78 @@
+//
+//  SafetyViewController.swift
+//  AutoLayout
+//
+//  Created by Ty Tom on 7/26/17.
+//  Copyright © 2017 TyTom. All rights reserved.
+//
+
+import UIKit
+
+
+extension UIView {
+    
+    @IBInspectable var cornerRadius: CGFloat {
+        get {
+            return layer.cornerRadius
+        }
+        set {
+            layer.cornerRadius = newValue
+            layer.masksToBounds = newValue > 0
+        }
+    }
+    
+    @IBInspectable var borderWidth: CGFloat {
+        get {
+            return layer.borderWidth
+        }
+        set {
+            layer.borderWidth = newValue
+        }
+    }
+    
+    @IBInspectable var borderColor: UIColor? {
+        get {
+            return UIColor(cgColor: layer.borderColor!)
+        }
+        set {
+            layer.borderColor = newValue?.cgColor
+        }
+    }
+}
+
+class SafetyViewController: UIViewController {
+
+    
+    @IBOutlet weak var safety: UIButton!
+    /*
+        safety.backgroundColor = clear
+        safety.layer.cornerRadius = 5
+        safety.layer.borderWidth = 1
+        safety.layer.borderColor = UIColor.black.cgColor
+    
+    */
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ LocationViewController.swift
@@ -0,0 +1,61 @@
+//
+//  LocationViewController.swift
+//  AutoLayout
+//
+//  Created by Ty Tom on 7/26/17.
+//  Copyright © 2017 TyTom. All rights reserved.
+//
+
+
+
+import UIKit
+import CoreLocation
+import MapKit
+
+class LocationViewController: UIViewController, CLLocationManagerDelegate, MKMapViewDelegate {
+    
+    @IBOutlet weak var MKMapView: MKMapView!
+
+    var locationManager = CLLocationManager()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.startUpdatingLocation()
+        
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        
+        let userLocation: CLLocation = locations[0]
+        
+        let latitude = userLocation.coordinate.latitude
+        
+        let longitude = userLocation.coordinate.longitude
+        
+        let latDelta: CLLocationDegrees = 0.05
+        
+        let lonDelta: CLLocationDegrees = 0.05
+        
+        let span = MKCoordinateSpan(latitudeDelta: latDelta, longitudeDelta: lonDelta)
+        
+        let location = CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
+        
+        let region = MKCoordinateRegion(center: location, span: span)
+        
+        self.MKMapView.setRegion(region, animated: true)
+        
+        
+    }
+    
+    
+}
